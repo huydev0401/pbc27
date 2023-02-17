03995ba69c16d61d9a708fda38adfd1235386552
huydev0401 <quanghuynguyennqh@gmail.com>
2023-02-17 13:20:21 +0700

    Update code

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Hero() {
   return (
-    <section className="bg-[url('../public/images/hero.png')] bg-right md:bg-top bg-no-repeat bg-cover pt-[130px] h-[695px]">
+    <section className="bg-[url('../public/images/banner-renew.jpg')] bg-right md:bg-top bg-no-repeat bg-cover pt-[130px] h-[695px]">
       <div className="page-container md:pl-[90px]">
         <div className="mt-[100px] md:mt-0 mb-[30px] md:mb-[60px]">
           <img srcSet="/images/pbc-logo.png 2x" alt="" />
